ec79bfef204311da5521d6a72b14fee37aa1e5b3
shyleshnandan <82893242+shyleshnandan@users.noreply.github.com>
2023-07-15 22:46:25 +0530

    Add files via upload

---
+++ quizapp/quizapp/src/main/java/com/shylesh/quizapp/QuizappApplication.java
@@ -0,0 +1,13 @@
+package com.shylesh.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizappApplication.class, args);
+	}
+
+}
